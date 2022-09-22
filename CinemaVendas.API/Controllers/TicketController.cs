52c7da3180187bc76def1f4c1a59f8ee837816a8
Castelani <kaue1castelani@gmail.com>
2022-09-22 20:53:41 -0300

    Inicio

---
+++ CinemaVendas.API/Controllers/TicketController.cs
@@ -0,0 +1,23 @@
+﻿using CinemaVendas.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaVendas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly ITicketService _ticketService;
+
+        public TicketController(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+
+        [HttpGet]
+        public IActionResult GetTickets()
+        {
+            return Ok(_ticketService.GetAllSold());
+        }
+    }
+}
