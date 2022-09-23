f99d877554528ca90d3f1e7491ef60698e5bd968
Castelani <kaue1castelani@gmail.com>
2022-09-22 22:00:59 -0300

    testesatividade

---
+++ CinemaVendas.API.Test/FinancialControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using CinemaVendas.API.Controllers;
+using CinemaVendas.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CinemaVendas.API.Test
+{
+    public class FinancialControllerTest
+    {
+        private Mock<IFinancialsService> _mockService;
+        private FinancialController _controller;
+
+
+
+        public FinancialControllerTest()
+        {
+            _mockService = new Mock<IFinancialsService>();
+            _controller = new FinancialController(_mockService.Object);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(31.6)]
+        [InlineData(-31.6)]
+        [InlineData(-1)]
+        public void GetTotalSold_HasAmout_ReturnOkResult(decimal total)
+        {
+            //Arrange
+            _mockService.Setup(service => service.GetTotalSold()).Returns(total);
+            //ACT
+            var result = _controller.GetTotalSold();
+            //ASSERT
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var numbers = Assert.IsType<Decimal>(okResult.Value);
+    
+        }
+    }
+}
--- CinemaVendas.API.Test/TicketControllerTest.cs
+++ CinemaVendas.API.Test/TicketControllerTest.cs
@@ -1,13 +1,49 @@
-﻿using System;
+﻿using CinemaVendas.API.Controllers;
+using CinemaVendas.Core.Models;
+using CinemaVendas.Core.Repositories.Interfaces;
+using CinemaVendas.Core.Services;
+using CinemaVendas.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace CinemaVendas.API.Test
 {
-    class TicketControllerTest
+    public class TicketControllerTest
     {
+        Mock<ITicketService> mockService;
+        TicketController ticketController;
 
+        public TicketControllerTest()
+        {
+            mockService = new Mock<ITicketService>();
+            ticketController = new TicketController(mockService.Object);
+        }
+
+        [Fact]
+        public void GetFoodSold_WithOutData_ReturnOkResult()
+        {
+            //arrange
+            mockService.Setup(service => service.GetAllSold())
+                .Returns(new List<Ticket>()
+                {
+                    new Ticket(){ID = 999, MovieName = "Capitao Caverna"},
+                    new Ticket(){ID = 587, MovieName = "Homem das Neves"}
+                });
+            //act
+            var result = ticketController.GetTickets();
+            //assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var foodItems = Assert.IsType<List<FoodItem>>(okResult.Value);
+            Assert.Equal(2, foodItems.Count);
+        }
     }
+    
+
+      
 }
